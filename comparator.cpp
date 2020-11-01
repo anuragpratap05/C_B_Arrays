b4be268f8b0bd3474b0b9cc4acc2b01aa8390c9d
anuragpratap05 <56753626+anuragpratap05@users.noreply.github.com>
2020-11-01 08:09:45 +0530

    Create comparator.cpp

---
+++ comparator.cpp
@@ -0,0 +1,27 @@
+# C_B_Arrays
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(int a, int b)
+{
+    return a>b;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[1000];
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    sort(a,a+n,compare);
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
